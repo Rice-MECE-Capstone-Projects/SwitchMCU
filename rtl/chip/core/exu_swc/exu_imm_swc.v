b80ab349ac201f4b83a774fef026b48cf20791f9
lindseyr23 <lindsey.russ05@gmail.com>
2024-10-01 14:54:15 -0500

    added test bench files and run sim files

--- rtl/chip/core/exu_swc/exu_imm_swc.v
+++ rtl/chip/core/exu_swc/exu_imm_swc.v
@@ -23,7 +23,9 @@ module ex_imm_en (
     reg_ren_1                                   ,
     reg_rdata_1                                 
 );
-
+/*
+This module performs Integer-Register Immediate instructions, placing the result in the dec_rd register.
+*/
 // general signals
 input                       hclk                ;
 input                       hrstn               ;
@@ -68,6 +70,7 @@ assign reg_ren_1   = mid_reg_ren_1 ? mid_reg_ren_1    : 'z  ;
 // the process
 always@(posedge hclk or negedge hrstn) begin
     if(!hrstn) begin
+    	// on a reset, set all intermediate values to 0
         mid_reg_raddr_1 <= 0;
         mid_reg_ren_1   <= 0;
         
@@ -75,47 +78,47 @@ always@(posedge hclk or negedge hrstn) begin
         mid_reg_wen     <= 0;
         mid_reg_wdata   <= 0;
     end else if(dec_branch_en)begin
-        if(cycle_cnt == 1) begin
+        if(cycle_cnt == 1) begin // on cycle 1 assign the temp read address to the decoded address, and enable writing to the read address
             mid_reg_raddr_1 <= dec_rs1;
             mid_reg_ren_1   <= 1;
 
             mid_reg_waddr   <= 0;
             mid_reg_wen     <= 0;
             mid_reg_wdata   <= 0;
-        end else if(cycle_cnt == 2) begin
+        end else if(cycle_cnt == 2) begin // on cycle 2 clear the intermediate values.
             mid_reg_raddr_1 <= 0;
             mid_reg_ren_1   <= 0;
 
             mid_reg_waddr   <= 0;
             mid_reg_wen     <= 0;
             mid_reg_wdata   <= 0;
-        end else if(cycle_cnt == 3)begin
+        end else if(cycle_cnt == 3)begin // on cycle 3 assign the write address to the destination register and enable writing to the register.
             mid_reg_raddr_1 <= 0;
             mid_reg_ren_1   <= 0;
 
             mid_reg_waddr   <= dec_rd;
             mid_reg_wen     <= 1;
-            if(dec_addi)
+            if(dec_addi) // for ADDI add the immediate to the data
                 mid_reg_wdata <= {{20{dec_imm_type_i[11]}}, dec_imm_type_i} + reg_rdata_1;
-            else if(dec_slti)
+            else if(dec_slti) // for SLTI check if the data is less than the immediate for signed values
                 mid_reg_wdata <= {31'b0, $signed(reg_rdata_1) < $signed({{20{dec_imm_type_i[11]}}, dec_imm_type_i})};
-            else if(dec_sltiu)
+            else if(dec_sltiu) // for SLTIU check if the data is less than the immediate for unsigned values
                 mid_reg_wdata <= {31'b0, reg_rdata_1 < {{20{dec_imm_type_i[11]}}, dec_imm_type_i}};
-            else if(dec_xori)
+            else if(dec_xori) // for XORI take the bitwise XOR of the data and immediate
                 mid_reg_wdata <= reg_rdata_1 ^ {{20{dec_imm_type_i[11]}}, dec_imm_type_i};
-            else if(dec_ori)
+            else if(dec_ori) // for ORI take the bitwise OR of the data and immediate
                 mid_reg_wdata <= reg_rdata_1 | {{20{dec_imm_type_i[11]}}, dec_imm_type_i};
-            else if(dec_andi)
+            else if(dec_andi) // for ANDI take the bitwise AND of the data and immediate
                 mid_reg_wdata <= reg_rdata_1 & {{20{dec_imm_type_i[11]}}, dec_imm_type_i};
-            else if(dec_slli)
+            else if(dec_slli) // for SLLI perform a logical left shift of the data by the immediate value
                 mid_reg_wdata <= reg_rdata_1 << dec_imm_type_i[4:0];
-            else if(dec_srli)
+            else if(dec_srli) // for SRLI perform a logical right shift of the data by the immediate value
                 mid_reg_wdata <= reg_rdata_1 >> dec_imm_type_i[4:0];
-            else if(dec_srai)
+            else if(dec_srai) // for SRAI perform an arithmetic right shift of the data by the immediate value
                 mid_reg_wdata <= $signed(reg_rdata_1) >>> dec_imm_type_i[4:0];
             else
                 mid_reg_wdata <= 0;
-            end else begin
+            end else begin // on cycle 4 set all the temp registers to 0
                 mid_reg_raddr_1 <= 0;
                 mid_reg_ren_1   <= 0;
     
@@ -123,7 +126,7 @@ always@(posedge hclk or negedge hrstn) begin
                 mid_reg_wen     <= 0;
                 mid_reg_wdata   <= 0;
             end
-    end else begin
+    end else begin // if dec_branch_en is not enabled, set all temp registers to 0
         mid_reg_raddr_1 <= 0;
         mid_reg_ren_1   <= 0;
 
