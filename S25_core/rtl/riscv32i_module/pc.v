c22b9d59135bbf3224c0b2f8c552bfa0d767c116
juan Garza <JuanArturoGarza@gmail.com>
2025-02-09 20:28:25 -0600

    tested matrix mult

--- S25_core/rtl/riscv32i_module/pc.v
+++ S25_core/rtl/riscv32i_module/pc.v
@@ -19,7 +19,7 @@ assign pc_o = PC;
 always @(posedge clk_i) begin
 //     $display("Time: %3d\thalt: %d\tisTakenBranch: %d\tnextPC: %d\tpc_po: %d",$time, halt_pi, isTakenBranch_pi, PC, pc_po);
   	 if (reset_i)
-	    PC  <= 32'hCC;
+	    PC  <= 32'h3F8;
       //Starting memory address, this logic must be changed later
 	 else 
  	    if (change_PC_condition | ~halt_i)  begin
