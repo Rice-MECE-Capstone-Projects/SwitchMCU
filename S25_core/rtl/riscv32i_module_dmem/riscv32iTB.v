1bf88b2f0481c16c0c6ac2d66c42a0dff7b6cf7d
juan Garza <JuanArturoGarza@gmail.com>
2025-02-17 01:18:14 -0600

    added stalling logic

--- S25_core/rtl/riscv32i_module_dmem/riscv32iTB.v
+++ S25_core/rtl/riscv32i_module_dmem/riscv32iTB.v
@@ -1,140 +1,5 @@
-// This is a basic testbench for StreamingHistogram assignment.
-
 `timescale 1ps / 1ps
 `include "params.vh"
-
-// module riscv32iTB
-// #(
-//     parameter   N_param = 32 
-// )
-// ();
-//     glbl glbl ();
-//     localparam IDLE_LEN = 10;
-
-//     reg                      tb_clk;
-//     reg                      tb_reset;
-//     wire                     m_axis_aresetn;
-
-//     wire i_clk;
-//     wire i_en;
-//     wire [N_param-1:0]  instruction;
-//     wire  [4:0] rd_o;
-//     wire  [4:0] rs1_o;
-//     wire  [4:0] rs2_o;
-//     wire  [2:0] fun3_o;
-//     wire  [6:0] fun7_o;
-//     wire [31:0] imm_o;
-//     wire [6:0] INST_typ_o;
-
-
-
-
-
-//     riscv32i
-// `ifndef GATESIM
-// #(    .N_param(N_param)
-//       ) 
-// `endif
-//     dut (
-//         .clk(   tb_clk),
-//         .reset(tb_reset)
-// );
-
-
-//     always begin
-//         tb_clk = 1'b0;
-//         #5000;
-//         tb_clk = 1'b1;
-//         #5000;
-//     end
-
-//     initial begin : init
-//         //logic [32*8-1:0] vcdfile;
-//         string vcdfile;
-//         int vcdlevel;
-//         if ($value$plusargs("VCDFILE=%s",vcdfile))
-//             $dumpfile(vcdfile);
-//         if ($value$plusargs("VCDLEVEL=%d",vcdlevel))
-//             $dumpvars(vcdlevel);
-//             end
-
-
-//     initial begin : stim
-//         $display("%t: starting stream stimulus", $time);
-//         $display("%t: TEST PASSED", $time);
-//         // $finish;
-//     end
-
-
-
-//     // Clock generation
-//     // initial begin
-//     //     clk = 0;
-//     //     forever #5 clk = ~clk;  // Generate a clock with a period of 10 units
-//     // end
-
-//     // Simulation control
-//     initial begin
-//         tb_reset = 1;  // Start with reset asserted
-//         repeat (5) @(posedge tb_clk);
-//         tb_reset = 0;  // Release reset after 10 time units
-//         repeat (50) @(posedge tb_clk);
-
-//         $finish;
-//     end
-
-// //  always @(negedge tb_clk) begin : checker
-// //             $display("%t:      fun3_o:{%h}, fun7_o:{%h}          ",
-// //             $time,        fun3_o,     fun7_o            
-// //             );
-// //  end
-
-// //     always @(negedge tb_clk) begin : checker
-// //         if (transfer_active) begin 
-// //             $display("%t:                 m_tdata:{%h},  s_tlast: {%b}, s_tready: {%b} , m_tvalid: {%b}           ",
-// //             $time,        m_axis_tdata,         s_axis_tlast,       s_axis_tready,        m_axis_tvalid         
-// //             );
-// //             if (s_axis_tready && s_axis_tvalid) begin 
-// //             $display("%t:       Contoler to RTL, sent    {%h} ", 
-// //             $time,           s_axis_tdata_recived
-// //             );
-// //             end
-// //             if (m_axis_tready && m_axis_tvalid) begin 
-// //             $display("%t:      RTL to FIFO sent, recived {%h} ",
-// //             $time,      m_axis_tdata_recived,     
-// //             );
-// //             end
-// //         $display("__________________________________________________________________________________________________________________");
-// //         end
-// //     end
-
-// // task send_data____spacer();
-// // begin
-// //     @(posedge tb_clk);
-// //     #1000
-
-    
-// //     s_axis_tvalid <= 0;
-// //     s_axis_tdata <= 0;
-// //     s_axis_tlast <= 0;
-// //     tb_reset <= 1'b1;
-// //     s_axis_tkeep   <= {TDATA_BYTES{1'b1}};
-// //     m_axis_tready  <= 0;
-// //     $display("%t: SD Req, ____SPACER data: -- ,      last: -- ",
-// //                 $time);
-    
-// // end
-// // endtask
-
-
-
-
-
-//     endmodule
-
-    
-
-`timescale 1ns/1ps
 
 module riscv32iTB//;
 #(
@@ -152,29 +17,7 @@ module riscv32iTB//;
     reg [31:0] address;
     reg [31:0] storeData;
     wire [31:0] loadData_w;
-
-    // // Instruction definitions (encoded as example opcodes)
-    // localparam [63:0] inst_LB  = 64'b00000000000000000000000000000001; // Example opcode for LB
-    // localparam [63:0] inst_LH  = 64'b00000000000000000000000000000010; // Example opcode for LH
-    // localparam [63:0] inst_LW  = 64'b00000000000000000000000000000100; // Example opcode for LW
-    // localparam [63:0] inst_LBU = 64'b00000000000000000000000000001000; // Example opcode for LBU
-    // localparam [63:0] inst_LHU = 64'b00000000000000000000000000010000; // Example opcode for LHU
-    // localparam [63:0] inst_SB  = 64'b00000000000000000000000000100000; // Example opcode for SB
-    // localparam [63:0] inst_SH  = 64'b00000000000000000000000001000000; // Example opcode for SH
-    // localparam [63:0] inst_SW  = 64'b00000000000000000000000010000000; // Example opcode for SW
-
-    // DUT instantiation
-
-//     riscv32i
-// `ifndef GATESIM
-// #(    .N_param(N_param)
-//       ) 
-// `endif
-//     dut (
-//         .clk(   tb_clk),
-//         .reset(tb_reset)
-// );
-
+    reg [31:0] Cycle_count;
 
 dataMem
 `ifndef GATESIM
@@ -188,11 +31,16 @@ dataMem
         .Single_Instruction(Single_Instruction),
         .address(address),
         .storeData(storeData),
-        .loadData_w(loadData_w)
+        .loadData_w(loadData_w),
+        .stall_mem_not_avalible(stall_mem_not_avalible),
+        .load_into_reg(load_into_reg),
+        .Cycle_count(Cycle_count)
     );
+        // wire [31:0] Cycle_count;
+        wire stall_mem_not_avalible, load_into_reg;
 
     // Clock generation
-    always #5 clk = ~clk; // 100 MHz clock
+    always #5000 clk = ~clk; // 100 MHz clock
 
     // Test procedure
     initial begin
@@ -207,81 +55,131 @@ dataMem
         repeat (2) @(posedge clk);
         reset = 0;
         repeat (1) @(posedge clk);
+        store_operation(inst_SB, 32'h00000004, 32'hAA);
+        store_operation(inst_SB, 32'h00000008, 32'hAA);
+        store_operation(inst_SB, 32'h0000000C, 32'hAA);
+        load_operation(inst_LH,  32'h00000004);
 
-        // Test LB (Load Byte)
-        Single_Instruction = inst_LB;
-        address = 32'h00000004; // Address of the 2nd word, byte 0
+//         // Test LB (Load Byte)
+//         Single_Instruction = inst_LB;
+//         address = 32'h00000004; // Address of the 2nd word, byte 0
+//         repeat (1) @(posedge clk);
+//         $display("LB: Address=0x%h, LoadData=0x%h", address, loadData_w);
+
+//         // Test LH (Load Halfword)
+//         repeat (1) @(posedge clk);
+
+//         Single_Instruction = inst_LH;
+//         address = 32'h00000006*4; // Address of the 2nd word, halfword 1
+//         repeat (1) @(posedge clk);
+//         $display("LH: Address=0x%h, LoadData=0x%h", address, loadData_w);
+
+//         Single_Instruction = inst_LW;
+//         address = 32'h00000008; // Address of the 3rd word
+//         repeat (1) @(posedge clk); 
+//         $display("LW: Address=0x%h, LoadData=0x%h", address, loadData_w);
+
+//         // Test LBU (Load Byte Unsigned)
+        
+// repeat (1) @(posedge clk);
+// Single_Instruction = inst_LBU;
+//         address = 32'h00000004; // Address of the 2nd word, byte 0
+        
+// repeat (1) @(posedge clk);
+// $display("LBU: Address=0x%h, LoadData=0x%h", address, loadData_w);
+
+//         // Test LHU (Load Halfword Unsigned)
+        
+// repeat (1) @(posedge clk);
+// Single_Instruction = inst_LHU;
+//         address = 32'h00000006; // Address of the 2nd word, halfword 1
+        
+// repeat (1) @(posedge clk);
+// $display("LHU: Address=0x%h, LoadData=0x%h", address, loadData_w);
+
+//         // Test SB (Store Byte)
+        
+// repeat (1) @(posedge clk);
+// Single_Instruction = inst_SB;
+//         address = 32'h00000004; // Address of the 2nd word, byte 0
+//         storeData = 32'hAA; // Store byte value
+        
+// repeat (1) @(posedge clk);
+// $display("SB: Address=0x%h, StoredData=0x%h", address, storeData);
+
+//         // Test SH (Store Halfword)
+        
+// repeat (1) @(posedge clk);
+// Single_Instruction = inst_SH;
+//         address = 32'h00000006; // Address of the 2nd word, halfword 1
+//         storeData = 32'hBBCC; // Store halfword value
+        
+// repeat (1) @(posedge clk);
+// $display("SH: Address=0x%h, StoredData=0x%h", address, storeData);
+
+//         // Test SW (Store Word)
+        
+// repeat (1) @(posedge clk);
+
+//         Single_Instruction = inst_SW;
+//         address = 32'h00000008; // Address of the 3rd word
+//         storeData = 32'hDEADBEEF; // Store word value
+//         repeat (1) @(posedge clk);
+        
+//         $display("SW: Address=0x%h, StoredData=0x%h", address, storeData);
+
+//         // Finish simulation
+        
         repeat (1) @(posedge clk);
-        $display("LB: Address=0x%h, LoadData=0x%h", address, loadData_w);
-
-        // Test LH (Load Halfword)
-        repeat (1) @(posedge clk);
-
-        Single_Instruction = inst_LH;
-        address = 32'h00000006*4; // Address of the 2nd word, halfword 1
-        repeat (1) @(posedge clk);
-        $display("LH: Address=0x%h, LoadData=0x%h", address, loadData_w);
-
-        Single_Instruction = inst_LW;
-        address = 32'h00000008; // Address of the 3rd word
-        repeat (1) @(posedge clk); 
-        $display("LW: Address=0x%h, LoadData=0x%h", address, loadData_w);
-
-        // Test LBU (Load Byte Unsigned)
-        
-repeat (1) @(posedge clk);
-Single_Instruction = inst_LBU;
-        address = 32'h00000004; // Address of the 2nd word, byte 0
-        
-repeat (1) @(posedge clk);
-$display("LBU: Address=0x%h, LoadData=0x%h", address, loadData_w);
-
-        // Test LHU (Load Halfword Unsigned)
-        
-repeat (1) @(posedge clk);
-Single_Instruction = inst_LHU;
-        address = 32'h00000006; // Address of the 2nd word, halfword 1
-        
-repeat (1) @(posedge clk);
-$display("LHU: Address=0x%h, LoadData=0x%h", address, loadData_w);
-
-        // Test SB (Store Byte)
-        
-repeat (1) @(posedge clk);
-Single_Instruction = inst_SB;
-        address = 32'h00000004; // Address of the 2nd word, byte 0
-        storeData = 32'hAA; // Store byte value
-        
-repeat (1) @(posedge clk);
-$display("SB: Address=0x%h, StoredData=0x%h", address, storeData);
-
-        // Test SH (Store Halfword)
-        
-repeat (1) @(posedge clk);
-Single_Instruction = inst_SH;
-        address = 32'h00000006; // Address of the 2nd word, halfword 1
-        storeData = 32'hBBCC; // Store halfword value
-        
-repeat (1) @(posedge clk);
-$display("SH: Address=0x%h, StoredData=0x%h", address, storeData);
-
-        // Test SW (Store Word)
-        
-repeat (1) @(posedge clk);
-
-        Single_Instruction = inst_SW;
-        address = 32'h00000008; // Address of the 3rd word
-        storeData = 32'hDEADBEEF; // Store word value
-        repeat (1) @(posedge clk);
-        
-        $display("SW: Address=0x%h, StoredData=0x%h", address, storeData);
-
-        // Finish simulation
-        
-        repeat (1) @(posedge clk);
-        $writememh("memory_dump.hex", dut.DMEM);
+        // $writememh("memory_dump.hex", dut.DMEM);
 
         $finish;
     end
+
+    task store_operation;
+        input [63:0] instruction;
+        input [31:0] addr;
+        input [31:0] data;
+        begin
+            Single_Instruction = instruction;
+            address = addr;
+            storeData = data;
+                @(posedge clk);
+            #30
+
+            $display("STORE: Instruction=0x%h, Address=0x%h, StoredData=0x%h", instruction, addr, data);
+        end
+    endtask
+
+    // Load operation task
+    task load_operation;
+        input [63:0] instruction;
+        input [31:0] addr;
+        begin
+            Single_Instruction = instruction;
+            address = addr;
+             @(posedge clk);
+            #30
+            $display("LOAD: Instruction=0x%h, Address=0x%h, LoadData=0x%h", instruction, addr, loadData_w);
+        end
+    endtask
+always @(posedge         clk) begin
+        #1
+        if (Cycle_count > 0) begin 
+    $display("\n----------------------------------------------------------------------------------START\n");
+
+                $display("Cycle: %d",Cycle_count);// Instruction: %h, Address: %h, LoadData: %h", Cycle_count, Single_Instruction, address, loadData_w);
+        end
+// 242 198
+end
+always @(posedge clk) begin
+  	 if (reset)
+	    Cycle_count  <= 32'h0;
+	 else 
+            Cycle_count <= Cycle_count + 1;
+  end
+
+
+
 endmodule
 
