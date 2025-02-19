db14b2f01b26235ca330d15fe880f13816774bd6
juan Garza <JuanArturoGarza@gmail.com>
2025-02-19 10:49:51 -0600

    branch simulation C code change

--- S25_core/bare_c_code/riscv32_C_add_small_address_map/inputs/program.c
+++ S25_core/bare_c_code/riscv32_C_add_small_address_map/inputs/program.c
@@ -1,6 +1,7 @@
 #define PERIPHERAL_BASE           0x00000600  
 #define PERIPHERAL_SUM            0x00000604
 #define PERIPHERAL_SUM_immediate  0x00000608
+#define PERIPHERAL_SUM_i           0x0000060C
 
 
 void write_to_peripheral(int address, int value) {
@@ -18,9 +19,22 @@ int main() {
     write_to_peripheral(PERIPHERAL_SUM, sum);
 
     if (sum == 55) {
-                write_to_peripheral(PERIPHERAL_BASE, 0xDEADBEEF);
+                write_to_peripheral(PERIPHERAL_SUM_i,0xDEADF00F);
     } else {    write_to_peripheral(PERIPHERAL_BASE, 0x0BADF00D);}
-    while (1) {    }
+    
+    int sum_while;
+    sum_while = 0;
+    while (1) {   
+
+        sum_while += 1;
+        if (sum_while == 10) {
+            break;
+        }
+     }
+     write_to_peripheral(PERIPHERAL_BASE, 0xDEADBEEF);
+     while (1) {
+
+     }
     return 0; 
 }
 
